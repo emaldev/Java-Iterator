3417c0cc49166f9c905296be5bbcabb96ea2b882
emaldev <261611436+emaldev@users.noreply.github.com>
2026-06-22 03:00:32 +0430

    java ArrayList whith Scanner and sum whith Iterator

--- SumIteratorNumber.java
+++ SumIteratorNumber.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Iterator;
 public class SumIteratorNumber {
      public static void  main(String[] args){
+        Scanner input = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<Integer>();
+        int number;
+        
 
-        numbers.add(10);
-        numbers.add(20);
-        numbers.add(30);
-        numbers.add(40);
-        numbers.add(50);
+    
+        for(int i = 0; i  < 5; i++){
+            System.out.println("Enter number " + (i + 1) + ": ");
+            number = input.nextInt();
+            numbers.add(number);
+        }
            
         int sum = 0;
         Iterator<Integer> myIterator = numbers.iterator();
